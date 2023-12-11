b869c425bed68b2887982c94f53ecab6d05f44cd
cohaereo <cohaereo@protonmail.com>
2023-12-11 09:39:24 +0100

    quicktag: D1 strings

--- crates/quicktag/src/text.rs
+++ crates/quicktag/src/text.rs
@@ -13,11 +13,11 @@ use nohash_hasher::IntMap;
 
 use crate::packages::package_manager;
 
-// pub type TablePointer32<T> = _TablePointer<i32, u32, T>;
+pub type TablePointer32<T> = _TablePointer<i32, u32, T>;
 pub type TablePointer64<T> = _TablePointer<i64, u64, T>;
 pub type TablePointer<T> = TablePointer64<T>;
 
-// pub type RelPointer32<T = ()> = _RelPointer<i32, T>;
+pub type RelPointer32<T = ()> = _RelPointer<i32, T>;
 pub type RelPointer64<T = ()> = _RelPointer<i64, T>;
 pub type RelPointer<T = ()> = RelPointer64<T>;
 
@@ -53,9 +53,7 @@ where
 
         let seek64: i64 = offset.into();
         reader.seek(SeekFrom::Start(offset_base))?;
-        reader.seek(SeekFrom::Current(seek64))?;
-        // TODO(cohae): Check array header
-        reader.seek(SeekFrom::Current(16))?;
+        reader.seek(SeekFrom::Current(seek64 + 16))?;
 
         let count64: u64 = count.into();
         let mut data = Vec::with_capacity(count64 as _);
@@ -245,6 +243,42 @@ pub struct StringPart {
     pub _unk3: u32,
 }
 
+#[derive(BinRead, Debug)]
+pub struct StringContainerD1 {
+    pub file_size: u32,
+    pub string_hashes: TablePointer32<u32>,
+    pub language_english: TagHash,
+}
+
+#[derive(BinRead, Debug)]
+pub struct StringDataD1 {
+    #[br(assert(file_size < 0xf00000))]
+    pub file_size: u32,
+    pub string_parts: TablePointer32<StringPartD1>,
+    pub _unk1: (u32, u32),
+    pub _unk2: TablePointer32<()>,
+    pub string_data: TablePointer32<u8>,
+    pub string_combinations: TablePointer32<StringCombinationD1>,
+}
+
+#[derive(BinRead, Debug)]
+pub struct StringPartD1 {
+    pub _unk0: u32,
+    pub data: RelPointer32,
+    pub _unk1: u32,
+
+    pub byte_length: u16,
+    pub string_length: u16,
+    pub cipher_shift: u16,
+    pub _unk2: u16,
+}
+
+#[derive(BinRead, Debug)]
+pub struct StringCombinationD1 {
+    pub data: RelPointer32,
+    pub part_count: i32,
+}
+
 /// Expects raw un-shifted data as input
 pub fn decode_text(data: &[u8], cipher: u16) -> String {
     // cohae: Modern versions of D2 no longer use the cipher system, we can take a shortcut
@@ -281,20 +315,26 @@ pub fn decode_text(data: &[u8], cipher: u16) -> String {
 }
 
 pub fn create_stringmap() -> anyhow::Result<StringCache> {
-    if !matches!(
+    if matches!(
         package_manager().version,
         PackageVersion::Destiny2Shadowkeep
             | PackageVersion::Destiny2BeyondLight
             | PackageVersion::Destiny2WitchQueen
             | PackageVersion::Destiny2Lightfall
     ) {
+        create_stringmap_d2()
+    } else if package_manager().version == PackageVersion::DestinyTheTakenKing {
+        create_stringmap_d1()
+    } else {
         warn!(
             "{:?} does not support string loading",
             package_manager().version
         );
-        return Ok(StringCache::default());
-    };
+        Ok(StringCache::default())
+    }
+}
 
+pub fn create_stringmap_d2() -> anyhow::Result<StringCache> {
     let prebl = package_manager().version == PackageVersion::Destiny2Shadowkeep;
     let mut tmp_map: IntMap<u32, HashSet<String>> = Default::default();
     for (t, _) in package_manager()
@@ -328,7 +368,51 @@ pub fn create_stringmap() -> anyhow::Result<StringCache> {
                 final_string += &decode_text(&data, part.cipher_shift);
             }
 
-            // stringmap.insert(hash.0, final_string);
+            tmp_map.entry(*hash).or_default().insert(final_string);
+        }
+    }
+
+    Ok(tmp_map
+        .into_iter()
+        .map(|(k, v)| (k, v.into_iter().collect()))
+        .collect())
+}
+
+pub fn create_stringmap_d1() -> anyhow::Result<StringCache> {
+    let mut tmp_map: IntMap<u32, HashSet<String>> = Default::default();
+    for (t, _) in package_manager()
+        .get_all_by_reference(0x8080035a)
+        .into_iter()
+    {
+        let Ok(textset_header) = package_manager().read_tag_struct::<StringContainerD1>(t) else {
+            continue;
+        };
+
+        let Ok(data) = package_manager().read_tag(textset_header.language_english) else {
+            continue;
+        };
+        let mut cur = Cursor::new(&data);
+        let Ok(text_data) = cur.read_be::<StringDataD1>() else {
+            continue;
+        };
+
+        for (combination, hash) in text_data
+            .string_combinations
+            .iter()
+            .zip(textset_header.string_hashes.iter())
+        {
+            let mut final_string = String::new();
+
+            for ip in 0..combination.part_count {
+                cur.seek(combination.data.into())?;
+                cur.seek(SeekFrom::Current((ip as i64) * 20))?;
+                let part: StringPartD1 = cur.read_be()?;
+                cur.seek(part.data.into())?;
+                let mut data = vec![0u8; part.byte_length as usize];
+                cur.read_exact(&mut data)?;
+                final_string += &decode_text(&data, part.cipher_shift);
+            }
+
             tmp_map.entry(*hash).or_default().insert(final_string);
         }
     }
