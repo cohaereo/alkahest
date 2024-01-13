90cfdf5ac8ba88f416175b2c3009f49a984ab839
Froggy618157725 <froggy618157725@gmail.com>
2024-01-12 21:16:07 -0800

    Add Beacon to Utilities
    Beacons act as a navigational aid, allowing you to easily mark a location.
    The inspector panel allows you to teleport to or look towards beacons.

--- src/render/tween.rs
+++ src/render/tween.rs
@@ -1,4 +1,4 @@
-use glam::Vec3;
+use glam::{Vec2, Vec3};
 use std::time::Instant;
 
 #[derive(Clone)]
@@ -7,23 +7,36 @@ pub struct Tween {
     pub start: Vec3,
     pub end: Vec3,
     pub last_pos: Vec3,
+    pub start_angle: Vec2,
+    pub end_angle: Vec2,
+    pub last_angle: Vec2,
     pub start_time: Instant,
     pub duration: f32,
 }
 
 impl Tween {
-    pub fn new(func: fn(f32) -> f32, start: Vec3, end: Vec3, duration: f32) -> Self {
+    pub fn new(
+        func: fn(f32) -> f32,
+        start: Vec3,
+        end: Vec3,
+        start_angle: Vec2,
+        end_angle: Vec2,
+        duration: f32,
+    ) -> Self {
         Self {
             func,
             start,
             end,
             last_pos: start,
+            start_angle,
+            end_angle,
+            last_angle: start_angle,
             start_time: Instant::now(),
             duration,
         }
     }
 
-    pub fn update(&mut self) -> Vec3 {
+    pub fn update_pos(&mut self) -> Vec3 {
         let time = self.start_time.elapsed().as_secs_f32();
         let t = (time / self.duration).clamp(0., 1.);
         let s = (self.func)(t);
@@ -31,6 +44,16 @@ impl Tween {
         let new_pos = self.start.lerp(self.end, s);
         self.last_pos = new_pos;
         new_pos
+    }
+
+    pub fn update_angle(&mut self) -> Vec2 {
+        let time = self.start_time.elapsed().as_secs_f32();
+        let t = (time / self.duration).clamp(0., 1.);
+        let s = (self.func)(t);
+
+        let new_angle = self.start_angle.lerp(self.end_angle, s);
+        self.last_angle = new_angle;
+        new_angle
     }
 
     pub fn is_finished(&self) -> bool {
