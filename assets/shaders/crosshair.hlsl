d05b4f9eb1a609b2ae411a2137755b02cb3ff7cf
Froggy618157725 <froggy618157725@gmail.com>
2024-01-23 00:49:00 -0800

    Giving up on scaling the crosshair with window size...

--- assets/shaders/crosshair.hlsl
+++ assets/shaders/crosshair.hlsl
@@ -20,10 +20,8 @@ struct VSOutput {
     float4 position : SV_POSITION;
 };
 
-static float size = 0.008;
-static float width = 0.001;
-
-static float2 screenPos[10] = {
+/*
+static float2 screenPosW[10] = {
     float2(-size, width), // top left
     float2(-size, -width), // bottom left
     float2(size, width), // top right
@@ -37,14 +35,56 @@ static float2 screenPos[10] = {
     float2(width, size), // top right
     float2(width, -size), // bottom right
 };
+*/
+
+static float2 screenPosS[10] = {
+    float2(-1, 0), // top left
+    float2(-1, 0), // bottom left
+    float2(1, 0), // top right
+    float2(1, 0), // bottom right
+
+    float2(1, 0), //degenerate
+    float2(0, 1), //degenerate
+
+    float2(0, 1), // top left
+    float2(-0, -1), // bottom left
+    float2(0, 1), // top right
+    float2(0, -1), // bottom right
+};
+
+static float2 screenPosW[10] = {
+    float2(0, 1), // top left
+    float2(0, -1), // bottom left
+    float2(0, 1), // top right
+    float2(0, -1), // bottom right
+
+    float2(0, -1), //degenerate
+    float2(-1, 0), //degenerate
+
+    float2(-1, 0), // top left
+    float2(-1, 0), // bottom left
+    float2(1, 0), // top right
+    float2(1, 0), // bottom right
+};
 
 VSOutput VShader(uint vertexID : SV_VertexID) {
     VSOutput output;
-    float ratio = viewportSize.x/viewportSize.y;
-    // Let's make the width at least a pixel
-    float w = max(max(1.0/viewportSize.x, width), 1.0/(viewportSize.y * ratio)) / width;
 
-    output.position = float4(screenPos[vertexID].x * w, screenPos[vertexID].y*ratio * w, 0.0, 1.0);
+    float pixel_x = 1.0/viewportSize.x;
+    float pixel_y = 1.0/viewportSize.y;
+
+    // I'd like to scale this with window size,
+    // but haven't managed to make that work right...
+    float width = 2;
+    float size = 8.0 * width;
+
+    //float offset_x = (1 - (viewportSize.x % 2)) * pixel_x/2.0;
+    //float offset_y = (1 - (viewportSize.y % 2)) * pixel_y/2.0;
+
+    float pos_x = (screenPosW[vertexID].x * width + screenPosS[vertexID].x * size) * pixel_x;// + offset_x;
+    float pos_y = (screenPosW[vertexID].y * width + screenPosS[vertexID].y * size) * pixel_y;// + offset_y;
+
+    output.position = float4(pos_x, pos_y, 0.0, 1.0);
     return output;
 }
 
