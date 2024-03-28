f6a61dfc796f30daef1bfa98ce5070c2e6055bb1
cohaereo <cohaereo@protonmail.com>
2024-03-28 11:10:56 +0100

    Migrate to workspace layout

--- crates/alkahest/src/util/consts.rs
+++ crates/alkahest/src/util/consts.rs
@@ -4,7 +4,7 @@ pub const BUILD_DATE: &str = build_time_utc!("%Y-%m-%d");
 pub const BUILD_TIMESTAMP: &str = build_time_utc!();
 pub const GIT_HASH: &str = env!("GIT_HASH");
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
-pub const CHANGELOG_MD: &str = include_str!("../../CHANGELOG.md");
+pub const CHANGELOG_MD: &str = include_str!("../../../../CHANGELOG.md");
 
 pub const BANNER: &str = r#"
     :::     :::        :::    :::     :::     :::    ::: :::::::::: :::::::: ::::::::::: 
