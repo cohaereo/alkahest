9913fad0a2932be642c3368ec0d9cbbb0fdeee76
cohaereo <cohae@cohae.dev>
2024-05-20 22:49:38 +0200

    Add Discord link to help menu

--- crates/alkahest/src/gui/menu/mod.rs
+++ crates/alkahest/src/gui/menu/mod.rs
@@ -246,36 +246,36 @@ impl GuiView for MenuBar {
                         self.controls_open = true;
                         ui.close_menu()
                     }
-                    ui.separator();
-                    let update_channel = config::with(|c| c.update_channel);
-                    if ui
-                        .add_enabled(
-                            update_channel.is_some()
-                                && update_channel != Some(UpdateChannel::Disabled),
-                            egui::Button::new("Check for updates"),
-                        )
-                        .clicked()
-                    {
-                        if let Some(update_channel) = update_channel {
-                            resources
-                                .get_mut::<crate::updater::UpdateCheck>()
-                                .start(update_channel);
-                        }
-                        ui.close_menu();
-                    }
-
-                    if ui.button("Change update channel").clicked() {
-                        config::with_mut(|c| c.update_channel = None);
-                        ui.close_menu();
-                    }
-
-                    if let Some(update_channel) = update_channel {
-                        ui.label(format!(
-                            "Updates: {} {:?}",
-                            update_channel.icon(),
-                            update_channel
-                        ));
-                    }
+                    // ui.separator();
+                    // let update_channel = config::with(|c| c.update_channel);
+                    // if ui
+                    //     .add_enabled(
+                    //         update_channel.is_some()
+                    //             && update_channel != Some(UpdateChannel::Disabled),
+                    //         egui::Button::new("Check for updates"),
+                    //     )
+                    //     .clicked()
+                    // {
+                    //     if let Some(update_channel) = update_channel {
+                    //         resources
+                    //             .get_mut::<crate::updater::UpdateCheck>()
+                    //             .start(update_channel);
+                    //     }
+                    //     ui.close_menu();
+                    // }
+                    //
+                    // if ui.button("Change update channel").clicked() {
+                    //     config::with_mut(|c| c.update_channel = None);
+                    //     ui.close_menu();
+                    // }
+                    //
+                    // if let Some(update_channel) = update_channel {
+                    //     ui.label(format!(
+                    //         "Updates: {} {:?}",
+                    //         update_channel.icon(),
+                    //         update_channel
+                    //     ));
+                    // }
 
                     ui.separator();
 
@@ -298,6 +298,12 @@ impl GuiView for MenuBar {
 
                     if ui.button("Changelog").clicked() {
                         self.changelog_open = true;
+                        ui.close_menu();
+                    }
+                    if ui.button("Discord").clicked() {
+                        ui.ctx().open_url(egui::OpenUrl::new_tab(
+                            "https://discord.gg/PTR42Hc9BH".to_string(),
+                        ));
                         ui.close_menu();
                     }
                     if ui.button("About").clicked() {
