f9f0721343297c6c1bee2b6da6947282e5ef06f3
cohaereo <cohae@cohae.dev>
2024-07-18 09:01:37 +0200

    fix(test): Add stringcontainer to test_load_all_maps

--- crates/alkahest-test/src/maps.rs
+++ crates/alkahest-test/src/maps.rs
@@ -1,4 +1,6 @@
-use alkahest_data::map::SBubbleParent;
+use std::sync::Arc;
+
+use alkahest_data::{map::SBubbleParent, text::StringContainer};
 use alkahest_pm::package_manager;
 use alkahest_renderer::loaders::map::load_map;
 use futures::executor::block_on;
@@ -9,12 +11,19 @@ use crate::TestHarness;
 #[test]
 fn test_load_all_maps() {
     let harness = TestHarness::new();
+    let stringmap = Arc::new(StringContainer::default());
 
     let all_maps = package_manager().get_all_by_reference(SBubbleParent::ID.unwrap());
     let map_count = all_maps.len();
     for (i, (tag, _)) in all_maps.into_iter().enumerate() {
-        info!("Loading map {tag} {}/{map_count}", i+1);
-        let result = block_on(load_map(harness.renderer.clone(), tag, None, false));
+        info!("Loading map {tag} {}/{map_count}", i + 1);
+        let result = block_on(load_map(
+            harness.renderer.clone(),
+            tag,
+            None,
+            stringmap.clone(),
+            false,
+        ));
         if let Err(e) = result {
             // Workaround for encrypted maps
             if e.to_string()
