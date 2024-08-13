ee90f97a282086d8fae5ac47e255eaf969a6ba69
AndrisBorbas <andris.borbas@gmail.com>
2024-08-13 17:20:01 +0200

    feat: Outliner search

--- crates/alkahest/src/gui/outliner.rs
+++ crates/alkahest/src/gui/outliner.rs
@@ -17,7 +17,7 @@ use itertools::Itertools;
 use rustc_hash::FxHashMap;
 use strum::IntoEnumIterator;
 use winit::window::Window;
-
+use alkahest_renderer::ecs::Scene;
 use crate::{
     gui::{
         chip::EcsTagsExt,
@@ -32,6 +32,8 @@ pub struct OutlinerPanel {
     sort_by_distance: bool,
 
     filters: FxHashMap<EntityTag, bool>,
+
+    search: String,
 }
 
 impl Default for OutlinerPanel {
@@ -41,6 +43,7 @@ impl Default for OutlinerPanel {
             filters: EntityTag::iter()
                 .map(|tag| (tag, false))
                 .collect::<FxHashMap<_, _>>(),
+            search: "".to_string(),
         }
     }
 }
@@ -59,11 +62,43 @@ impl GuiView for OutlinerPanel {
 
             let camera = resources.get::<Camera>();
 
+            fn search(entity: Entity, s: &str, sce: &Scene) -> bool {
+                let e = sce.entity(entity);
+                let label = if let Some(label) = e.get::<Label>() {
+                    format!("{label}")
+                } else {
+                    return false;
+                };
+
+                let children = e.get::<Children>();
+                if let Some(children) = children {
+                    for child in children.iter() {
+                        if search(*child, s, sce) {
+                            return true;
+                        }
+                    }
+                }
+
+                if !label.to_lowercase().contains(&s) {
+                    return false;
+                }
+
+                true
+            }
+
             let enabled_filters = self.filters.iter().filter(|(_, v)| **v).count();
             let mut entities = scene
                 .query_filtered::<(Entity, Option<&Transform>, Option<&Tags>), Without<Parent>>()
                 .iter(scene)
-                .filter(|(_, _, tags)| {
+                .filter(|(e, _, tags)| {
+                    // Match search string
+                    if !self.search.is_empty() {
+                        let s = self.search.to_lowercase();
+                        if !search(*e, &s, scene){
+                            return false;
+                        }
+                    }
+
                     if enabled_filters == 0 {
                         return true;
                     }
@@ -97,6 +132,11 @@ impl GuiView for OutlinerPanel {
             // let mut delete_entity = None;
 
             egui::Window::new("Outliner").show(ctx, |ui| {
+                ui.horizontal(|ui| {
+                    ui.label("Search:");
+                    ui.add(egui::TextEdit::singleline(&mut self.search).hint_text("Search"));
+                });
+
                 ui.horizontal(|ui| {
                     ui.checkbox(&mut self.sort_by_distance, "Sort by distance");
 
