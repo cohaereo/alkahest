30695250edf4b40567de726f9ade73874579d239
cohae <cohae@cohae.dev>
2026-07-04 13:10:02 +0200

    feat: Apply exposure scale to atmosphere background

--- crates/render/builtin/shaders/blit_to_background.hlsl
+++ crates/render/builtin/shaders/blit_to_background.hlsl
@@ -11,6 +11,18 @@ cbuffer scope_view : register(b12) {
   // float4x4 camera_to_projective : packoffset(c11);
 }; // cbuffer scope_view
 
+cbuffer scope_frame : register(b13) {
+    float game_time;
+    float render_time;
+    float delta_game_time;
+    float exposure_time;
+
+    float exposure_scale;
+    float exposure_illum_relative_glow;
+    float exposure_scale_for_shading;
+    float exposure_illum_relative;
+};
+
 // Vertex Shader
 struct VSOutput {
   float4 pos : SV_POSITION;
@@ -49,5 +61,5 @@ float4 mainPS(VSOutput input) : SV_TARGET {
   float depth = deferred_depth.Sample(samplerState, input.uv).x;
   if (depth > 0)
     discard;
-  return source.Sample(samplerState, input.uv);
+  return source.Sample(samplerState, input.uv) * exposure_scale;
 }
