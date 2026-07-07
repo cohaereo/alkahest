6fabb4044aa7a107aad3a2a0def5e8e92d3cca91
cohae <cohae@cohae.dev>
2026-07-07 15:51:04 +0200

    fix: Hide console window in release builds

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 #[macro_use]
 extern crate tracing;
 use std::{fs::File, rc::Rc, sync::Mutex};
